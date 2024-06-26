50f3ff6fd3f53bffee01c82738dcb48a02436619
Weddy Nkatha <weddynkatha265@gmail.com>
2024-06-26 18:27:11 +0300

    Updates arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;  // Multiply num1 and num2 and assign the result to multiply
+
+console.log(multiply); // This will output 62
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 8;
+let num4 = 2;
+let mod = num3 / num4;
+console.log(mod)
+    
+ let max =   Math.max(1, 2, 3, 4, 20);
